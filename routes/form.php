77970434d76001ac9d1429b581cce5ec7811761a
TungDcode <TungDcode@users.noreply.github.com>
2023-04-13 15:06:00 +0000

    Fix PHP Syntax Styling

--- routes/form.php
+++ routes/form.php
@@ -3,6 +3,6 @@
 use Dcodegroup\FormBuilder\Http\Controllers\FormController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('/' . config('form-builder.route_path'), FormController::class, [
-    'names' => config('form-builder.route_name')
+Route::resource('/'.config('form-builder.route_path'), FormController::class, [
+    'names' => config('form-builder.route_name'),
 ]);
