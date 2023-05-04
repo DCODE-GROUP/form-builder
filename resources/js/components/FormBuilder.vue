c17d0fd99bdf2eb6fdf5e17e427c9d66f53da3f2
Tung Do <tung@dcodegroup.com>
2023-05-04 10:53:25 +0700

    Add paragraph

--- resources/js/components/FormBuilder.vue
+++ resources/js/components/FormBuilder.vue
@@ -40,6 +40,7 @@
               </li>
             </template>
           </draggable>
+          <slot></slot>
         </div>
         <div class="form-builder-fields">
           <draggable
@@ -56,80 +57,108 @@
               >
                 <div class="-field-title handle">
                   <h2 @click="element.isShowing = ! element.isShowing">
-                    <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"></path>
+                    <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"
+                         xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"></path>
                     </svg>
                     <span class="-title">
                         <span class="-type-title">{{ getFieldTypeTitle(element) }}:</span>
                         <span class="-name">{{ element.label }}</span>
                     </span>
                   </h2>
-                  <svg @click="removeField(index)" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                  <svg @click="removeField(index)" fill="none" stroke="currentColor" stroke-width="1.5"
+                       viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
                   </svg>
                 </div>
                 <div class="-field-properties">
-                  <div class="-prop -label-text">
-                    <span>Label</span>
-                    <input type="text" v-model="element.label"/>
-                  </div>
-                  <div class="-prop -placeholder" v-if="element.placeholder !== null">
-                    <span>Placeholder</span>
-                    <input type="text" v-model="element.placeholder"/>
-                  </div>
-                  <div class="-prop -width" v-if="element.class">
-                    <span class="-label">Width</span>
-                    <select v-model="element.class">
-                      <option value="small-12">Full</option>
-                      <option value="small-6">Half</option>
-                    </select>
-                  </div>
-                  <div class="-prop -options" v-if="element.options">
-                    <span class="-label">Options</span>
-                    <draggable
-                      :list="element.options"
-                      class="-added"
-                      :group="{ name: element.id, pull: false, put: false }"
-                      handle=".option-handle"
-                    >
-                      <template #item="{option, index}" :key="option.id">
-                        <div class="-option">
-                          <div class="option-handle">
-                            <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                              <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"></path>
-                            </svg>
-                          </div>
-                          <input v-model="element.options[index]" type="text"/>
-                          <button
-                            type="button"
-                            class="-remove"
-                            @click="removeFieldOption(option, index)"
-                          >
-                            <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
-                            </svg>
-                          </button>
-                        </div>
-                      </template>
-                    </draggable>
-                    <div class="-new">
-                      <input
-                        type="text"
-                        v-model="element.newOption"
-                        @keydown.enter.prevent="onAddOption(element)"
-                        placeholder="Add option"
-                      />
-                      <button
-                        type="button"
-                        class="button tiny"
-                        @click.prevent="onAddOption(element)"
-                      >
-                        <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                          <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                        </svg>
-                      </button>
+                  <template v-if="element.type === 'paragraph'">
+                    <div class="-full -label-text">
+                      <span>Content</span>
+                      <textarea cols="30" rows="3" v-model="element.content" :placeholder="element.placeholder"></textarea>
                     </div>
-                  </div>
+                    <div class="-prop">
+                      <span>Type</span>
+                      <select v-model="element.content_type">
+                        <option value="p">p</option>
+                        <option value="blockquote">blockquote</option>
+                        <option value="address">address</option>
+                      </select>
+                    </div>
+                    <div class="-prop -width">
+                      <span class="-label">Classes</span>
+                      <input v-model="element.class" type="text" name="classes" placeholder="Input space separated classes">
+                    </div>
+                  </template>
+                  <template v-else>
+                    <div class="-prop -label-text">
+                      <span>Label</span>
+                      <input type="text" v-model="element.label"/>
+                    </div>
+                    <div class="-prop -placeholder" v-if="element.placeholder !== null">
+                      <span>Placeholder</span>
+                      <input type="text" v-model="element.placeholder"/>
+                    </div>
+                    <div class="-prop -width" v-if="element.class">
+                      <span class="-label">Width</span>
+                      <select v-model="element.class">
+                        <option value="small-12">Full</option>
+                        <option value="small-6">Half</option>
+                      </select>
+                    </div>
+                    <div class="-prop -options" v-if="element.options">
+                      <span class="-label">Options</span>
+                      <draggable
+                        :list="element.options"
+                        class="-added"
+                        :group="{ name: element.id, pull: false, put: false }"
+                        handle=".option-handle"
+                      >
+                        <template #item="{option, index}" :key="option.id">
+                          <div class="-option">
+                            <div class="option-handle">
+                              <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"
+                                   xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"></path>
+                              </svg>
+                            </div>
+                            <input v-model="element.options[index]" type="text"/>
+                            <button
+                              type="button"
+                              class="-remove"
+                              @click="removeFieldOption(option, index)"
+                            >
+                              <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"
+                                   xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
+                              </svg>
+                            </button>
+                          </div>
+                        </template>
+                      </draggable>
+                      <div class="-new">
+                        <input
+                          type="text"
+                          v-model="element.newOption"
+                          @keydown.enter.prevent="onAddOption(element)"
+                          placeholder="Add option"
+                        />
+                        <button
+                          type="button"
+                          class="button tiny"
+                          @click.prevent="onAddOption(element)"
+                        >
+                          <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"
+                               xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                          </svg>
+                        </button>
+                      </div>
+                    </div>
+                  </template>
                 </div>
               </div>
             </template>
@@ -179,6 +208,15 @@ export default {
           label: "Heading",
           class: "small-12",
           placeholder: null,
+        },
+        {
+          name: "paragraph",
+          type: "paragraph",
+          label: "Paragraph",
+          content: "Paragraph",
+          content_type: "p",
+          class: "",
+          placeholder: "Input your paragraph",
         },
         {
           name: "text",
@@ -314,6 +352,8 @@ export default {
         type: template.type,
         label: template.label,
         placeholder: template.placeholder,
+        content: template.hasOwnProperty('content') ? template.content : '',
+        content_type: template.hasOwnProperty('content_type') ? template.content_type : '',
         class: template.class,
         options: template.options,
       };
@@ -334,6 +374,8 @@ export default {
             type: field.type,
             label: field.label,
             placeholder: field.placeholder,
+            content: field.hasOwnProperty('content') ? field.content : '',
+            content_type: field.hasOwnProperty('content_type') ? field.content_type : '',
             class: field.class,
             options: field.options,
           };
---
+++ resources/js/components/fields/Paragraph.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="paragraph">
+    <p v-if="field.content_type === 'p'" v-html="field.content"></p>
+    <blockquote v-if="field.content_type === 'blockquote'"><q v-html="field.content"></q></blockquote>
+    <address v-if="field.content_type === 'address'" v-html="field.content"></address>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VParagraph",
+  props: {
+    field: {},
+  },
+};
+</script>
+<style>
+blockquote {
+  display: block;
+  margin: 1em 40px;
+}
+</style>
