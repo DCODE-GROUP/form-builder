c4e194437575d1a841f08e2f45fb4f3408160dc5
Aaron Heath <aaron@aaronheath.com>
2024-07-11 09:36:26 +0930

    wip

---
+++ build/phpstan/cache/PHPStan/4c/be/4cbec5eaf9a9ea3bde1fe4e7bcc8c997529f1bc0.php
@@ -0,0 +1,159 @@
+<?php declare(strict_types = 1);
+
+// odsl-/home/aaronheath/code/dcode-form-builder/src
+return \PHPStan\Cache\CacheItem::__set_state(array(
+   'variableKey' => 'v1',
+   'data' => 
+  array (
+    '/home/aaronheath/code/dcode-form-builder/src/Models/Form.php' => 
+    array (
+      0 => 'c2cf4355929e09c285dac11fffc8a07a7d1f6a45',
+      1 => 
+      array (
+        0 => 'dcodegroup\\formbuilder\\models\\form',
+      ),
+      2 => 
+      array (
+        0 => 'dcodegroup\\formbuilder\\models\\data',
+        1 => 'dcodegroup\\formbuilder\\models\\fieldsjson',
+        2 => 'dcodegroup\\formbuilder\\models\\setfields',
+        3 => 'dcodegroup\\formbuilder\\models\\clearfields',
+        4 => 'dcodegroup\\formbuilder\\models\\get',
+        5 => 'dcodegroup\\formbuilder\\models\\savemodel',
+        6 => 'dcodegroup\\formbuilder\\models\\prefill',
+        7 => 'dcodegroup\\formbuilder\\models\\getfieldvalue',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+    '/home/aaronheath/code/dcode-form-builder/src/Models/Traits/HasFilledForms.php' => 
+    array (
+      0 => 'ea8abdbdde8cc5196bbe6e1b78886f2370e09397',
+      1 => 
+      array (
+        0 => 'dcodegroup\\formbuilder\\models\\traits\\hasfilledforms',
+      ),
+      2 => 
+      array (
+        0 => 'dcodegroup\\formbuilder\\models\\traits\\filledforms',
+        1 => 'dcodegroup\\formbuilder\\models\\traits\\getformdata',
+        2 => 'dcodegroup\\formbuilder\\models\\traits\\saveformdata',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+    '/home/aaronheath/code/dcode-form-builder/src/Models/FormData.php' => 
+    array (
+      0 => 'b7ee88183a3bec0bbc15b56081d327475e7fc470',
+      1 => 
+      array (
+        0 => 'dcodegroup\\formbuilder\\models\\formdata',
+      ),
+      2 => 
+      array (
+        0 => 'dcodegroup\\formbuilder\\models\\form',
+        1 => 'dcodegroup\\formbuilder\\models\\scopecompleted',
+        2 => 'dcodegroup\\formbuilder\\models\\prefillform',
+        3 => 'dcodegroup\\formbuilder\\models\\iscomplete',
+        4 => 'dcodegroup\\formbuilder\\models\\removeuploadedfile',
+        5 => 'dcodegroup\\formbuilder\\models\\filluploadedmedia',
+        6 => 'dcodegroup\\formbuilder\\models\\getformvalue',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+    '/home/aaronheath/code/dcode-form-builder/src/FormBuilderServiceProvider.php' => 
+    array (
+      0 => 'b17c424f0f8520c79086ec6e6464a162ff660f2a',
+      1 => 
+      array (
+        0 => 'dcodegroup\\formbuilder\\formbuilderserviceprovider',
+      ),
+      2 => 
+      array (
+        0 => 'dcodegroup\\formbuilder\\boot',
+        1 => 'dcodegroup\\formbuilder\\register',
+        2 => 'dcodegroup\\formbuilder\\registercommands',
+        3 => 'dcodegroup\\formbuilder\\offerpublishing',
+        4 => 'dcodegroup\\formbuilder\\doesnthavetables',
+        5 => 'dcodegroup\\formbuilder\\registerresources',
+        6 => 'dcodegroup\\formbuilder\\registerroutes',
+      ),
+      3 => 
+      array (
+        0 => 'FORM_BUILDER_PATH',
+      ),
+    ),
+    '/home/aaronheath/code/dcode-form-builder/src/Http/Requests/StoreFormRequest.php' => 
+    array (
+      0 => '18abbe5c1d03d11312bf02bf55f3a6df3c38ce88',
+      1 => 
+      array (
+        0 => 'dcodegroup\\formbuilder\\http\\requests\\storeformrequest',
+      ),
+      2 => 
+      array (
+        0 => 'dcodegroup\\formbuilder\\http\\requests\\authorize',
+        1 => 'dcodegroup\\formbuilder\\http\\requests\\rules',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+    '/home/aaronheath/code/dcode-form-builder/src/Http/Controllers/FormController.php' => 
+    array (
+      0 => 'c5509a7f4ac861d63449138ed0226da147a80413',
+      1 => 
+      array (
+        0 => 'dcodegroup\\formbuilder\\http\\controllers\\formcontroller',
+      ),
+      2 => 
+      array (
+        0 => 'dcodegroup\\formbuilder\\http\\controllers\\index',
+        1 => 'dcodegroup\\formbuilder\\http\\controllers\\show',
+        2 => 'dcodegroup\\formbuilder\\http\\controllers\\create',
+        3 => 'dcodegroup\\formbuilder\\http\\controllers\\store',
+        4 => 'dcodegroup\\formbuilder\\http\\controllers\\edit',
+        5 => 'dcodegroup\\formbuilder\\http\\controllers\\update',
+        6 => 'dcodegroup\\formbuilder\\http\\controllers\\destroy',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+    '/home/aaronheath/code/dcode-form-builder/src/Http/Traits/FormValidator.php' => 
+    array (
+      0 => '61445613363c8086ed4f5d3bdce04ddefb59f02c',
+      1 => 
+      array (
+        0 => 'dcodegroup\\formbuilder\\http\\traits\\formvalidator',
+      ),
+      2 => 
+      array (
+        0 => 'dcodegroup\\formbuilder\\http\\traits\\validate',
+        1 => 'dcodegroup\\formbuilder\\http\\traits\\getvalue',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+    '/home/aaronheath/code/dcode-form-builder/src/Commands/InstallCommand.php' => 
+    array (
+      0 => '1583139a29ddf88c4318dba3c84ef426e4f6901c',
+      1 => 
+      array (
+        0 => 'dcodegroup\\formbuilder\\commands\\installcommand',
+      ),
+      2 => 
+      array (
+        0 => 'dcodegroup\\formbuilder\\commands\\handle',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+  ),
+));
---
+++ build/phpstan/cache/PHPStan/67/b2/67b27b4216d502f96400c68cbce5373ac3773b4c.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types = 1);
+
+// odsl-/home/aaronheath/code/dcode-form-builder/database
+return \PHPStan\Cache\CacheItem::__set_state(array(
+   'variableKey' => 'v1',
+   'data' => 
+  array (
+    '/home/aaronheath/code/dcode-form-builder/database/migrations/create_forms_table.stub.php' => 
+    array (
+      0 => 'e56dd0c935cb8f9fec0fc202db505a8fdb559ed9',
+      1 => 
+      array (
+        0 => 'createformstable',
+      ),
+      2 => 
+      array (
+        0 => 'up',
+        1 => 'down',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+    '/home/aaronheath/code/dcode-form-builder/database/migrations/create_form_data_table.stub.php' => 
+    array (
+      0 => '213134b52330a8369829b8ebf3de96c0bf750742',
+      1 => 
+      array (
+        0 => 'createformdatatable',
+      ),
+      2 => 
+      array (
+        0 => 'up',
+        1 => 'down',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+  ),
+));
