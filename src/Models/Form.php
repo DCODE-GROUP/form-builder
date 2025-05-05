d6c5893e28629e3cd09aac3a3489a1f3ad9687ed
Tung Do <tung@dcodegroup.com>
2025-05-05 15:46:52 +0700

    Fix stan

--- src/Models/Form.php
+++ src/Models/Form.php
@@ -2,6 +2,7 @@
 
 namespace Dcodegroup\FormBuilder\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -9,8 +10,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * @property string $title
- * @property string|null $published_at
- * @property string $updated_at
+ * @property Carbon|null $published_at
+ * @property Carbon $updated_at
  * @property array $fields
  */
 class Form extends Model
